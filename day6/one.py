82eade43dfd208e37ab3e9df2106fe667103e205
Komol <tegirmon@gmail.com>
2019-12-06 22:37:01 -0500

    day 6 extra - plot the graph

--- day6/one.py
+++ day6/one.py
@@ -1,5 +1,6 @@
 import csv
 import networkx as nx
+import matplotlib.pyplot as plt
 
 G = nx.DiGraph()
 with open('in.txt') as input_file:
@@ -8,3 +9,8 @@ with open('in.txt') as input_file:
         G.add_edges_from([row])
 
 print(sum([len(nx.ancestors(G, n)) for n in G.nodes]))
+
+# extra
+labels = {"COM": "CENTER", "YOU": "ME", "SAN": "SAN"}
+nx.draw(G, node_size=10, width=0.2, font_color='r', labels=labels, with_labels=True)
+plt.show()
