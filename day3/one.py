78d6bbd8068b8d5499ac5ffe9e8e9a86c6f768cd
Komol <tegirmon@gmail.com>
2019-12-07 23:33:38 -0500

    day 3 - simplify

--- day3/one.py
+++ day3/one.py
@@ -1,46 +1,35 @@
 import csv
 
 
-def get_key(x, y):
-    return str(x)+"."+str(y)
+def get_key_val(x, y):
+    return str(x)+"_"+str(y), abs(x) + abs(y)
 
 
-c_list = []
-wire_num = 0
-cross_list = []
+def left_or_right(direction):
+    return 0 if direction not in ['R', 'L'] else 1 if direction == 'R' else -1
+
+
+def up_or_down(direction):
+    return 0 if direction not in ['U', 'D'] else 1 if direction == 'U' else -1
+
+
+def path_for(last_pos, direction, steps):
+    next_pos = last_pos + (steps * direction)
+    return next_pos, [] if direction == 0 else range(last_pos + direction, (next_pos + direction), direction)
+
+
+def get_wire(paths):
+    wire, last_x, last_y = {}, 0, 0
+    for path in paths:
+        direction, steps = path[0], int(path[1:])
+        last_x, rg = path_for(last_x, left_or_right(direction), steps)
+        wire.update([get_key_val(x, last_y) for x in rg])
+        last_y, rg = path_for(last_y, up_or_down(direction), steps)
+        wire.update([get_key_val(last_x, y) for y in rg])
+    return wire
+
+
 with open('in.txt') as input_file:
-    for row in csv.reader(input_file):
-        last_x = 0
-        last_y = 0
-        c_list.append({})
-        for move in row:
-            dx = 0
-            dy = 0
-            if move[0] == 'U':
-                dy = +1
-            elif move[0] == 'D':
-                dy = -1
-            elif move[0] == 'R':
-                dx = +1
-            elif move[0] == 'L':
-                dx = -1
-
-            steps = int(move[1:])
-
-            if dx != 0:
-                next_x = last_x + (steps * dx)
-                for x in range(last_x + dx, (next_x + dx), dx):
-                    c_list[wire_num][get_key(x, last_y)] = True
-                    if wire_num == 1 and (get_key(x, last_y) in c_list[0].keys()):
-                        cross_list.append(abs(x) + abs(last_y))
-                last_x = next_x
-            if dy != 0:
-                next_y = last_y + (steps * dy)
-                for y in range(last_y + dy, (next_y + dy), dy):
-                    c_list[wire_num][get_key(last_x, y)] = True
-                    if wire_num == 1 and (get_key(last_x, y) in c_list[0].keys()):
-                        cross_list.append(abs(last_x) + abs(y))
-                last_y = next_y
-        wire_num += 1
-
-print(min(cross_list))
+    first_wire = get_wire(next(csv.reader(input_file)))
+    second_wire = get_wire(next(csv.reader(input_file)))
+print(min([first_wire[point] for point in set(first_wire).intersection(set(second_wire))]))
